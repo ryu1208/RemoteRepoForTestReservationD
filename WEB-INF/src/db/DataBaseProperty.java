f8196a13140c3f26648187e331a5b7affe67951e
t010318 <t010318@ics857.icsuser.nagaokaut.ac.jp>
2016-11-24 19:39:51 +0900

    Story03 完了

--- WEB-INF/src/db/DataBaseProperty.java
+++ WEB-INF/src/db/DataBaseProperty.java
@@ -3,9 +3,9 @@ package db;
 public class DataBaseProperty {
 	static final public String DB_URL = "jdbc:sqlserver://mis.nagaokaut.ac.jp";
 
-	static final public String DB_NAME = "fyoshida";
+	static final public String DB_NAME = "SeTeamD";
 
-	static final public String DB_USERNAME = "fyoshida";
+	static final public String DB_USERNAME = "SeTeamD";
 
-	static final public String DB_PASSWORD = "fyoshida";
+	static final public String DB_PASSWORD = "SeTeamD";
 }
